ac635050e20dff4ed2477d5c07bcb07f5bcc6b96
Alfina Megasiwi <alfina.megasiwi@ui.ac.id>
2021-07-23 12:21:47 +0700

    deploy

--- panpacation/settings.py
+++ panpacation/settings.py
@@ -105,7 +105,7 @@ DATABASES = {
 if PRODUCTION:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'ENGINE': 'django.db.backends.postgresql',
             'NAME': os.environ.get('DATABASE_NAME'),
             'USER': os.environ.get('DATABASE_USER'),
             'PASSWORD': os.environ.get('DATABASE_PASS'),
